703af3d7d2464df84786d05fbdf474348cbf4f1c
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-08-29 16:08:54 -0300

    Creating OnBoarding ViewModel

---
+++ lib/presentation/onboarding/onboarding_view_model.dart
@@ -0,0 +1,44 @@
+import 'package:tut_app/presentation/base/base_view_model.dart';
+
+class OnBoardingViewModel extends BaseViewModel
+    with OnBoardingViewModelInputs, OnBoardingViewModelOutputs {
+  // stream controllers
+
+  // inputs
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  void start() {
+    // TODO: implement start
+  }
+
+  @override
+  void goNext() {
+    // TODO: implement goNext
+  }
+
+  @override
+  void goPrevious() {
+    // TODO: implement goPrevious
+  }
+
+  @override
+  void onPageChanged(int index) {
+    // TODO: implement onPageChanged
+  }
+}
+
+// inputs mean the orders that our view model will receive from our view
+abstract class OnBoardingViewModelInputs {
+  void goNext(); // when user clicks on right arrow or swipe left.
+  void goPrevious(); // when user clicks on left arrow or swipe right.
+  void onPageChanged(int index);
+}
+
+// outputs mean data or results that will be sent from our view model to our view
+abstract class OnBoardingViewModelOutputs {
+  // will be implement it later
+}
