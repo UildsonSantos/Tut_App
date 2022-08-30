a506432eb32671419cc634ebb9d49cb131abc9b7
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-08-30 16:11:45 -0300

    Data Layer Remote Data Source

---
+++ lib/data/data_source/remote_data_source.dart
@@ -0,0 +1,23 @@
+import 'package:tut_app/data/network/app_api.dart';
+import 'package:tut_app/data/request/request.dart';
+import 'package:tut_app/data/responses/responses.dart';
+
+abstract class RemoteDataSource {
+  Future<AuthenticationResponse> login(LoginRequest loginRequest);
+}
+
+class RemoteDataSourceImpl implements RemoteDataSource {
+  final AppServiceClient _appServiceClient;
+
+  RemoteDataSourceImpl(this._appServiceClient);
+
+  @override
+  Future<AuthenticationResponse> login(LoginRequest loginRequest) async {
+    return await _appServiceClient.login(
+      loginRequest.email,
+      loginRequest.password,
+      loginRequest.imei,
+      loginRequest.deviceType,
+    );
+  }
+}
