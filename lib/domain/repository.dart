30bd82518d1c5b008790a2f8366add636bf0f646
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-08-30 16:07:50 -0300

    Domain Layer Adding Repository Request and Failure

---
+++ lib/domain/repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:tut_app/data/network/failure.dart';
+import 'package:tut_app/data/request/request.dart';
+import 'package:tut_app/domain/models.dart';
+
+abstract class Repository {
+  Future<Either<Failure, Authentication>> login(LoginRequest loginRequest);
+}
