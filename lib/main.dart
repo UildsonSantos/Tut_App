3125bb60d5b24a5224333fbb931bc759f3e1fa3c
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-08-31 09:50:32 -0300

    Add dependency injection Instances

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:tut_app/app/app.dart';
 
-void main() {
+import 'app/di.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initAppModule();
   runApp(MyApp());
 }
