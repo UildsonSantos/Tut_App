162ec791e53b0ce809435772d78dd09bb221b7bf
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-08-31 00:38:17 -0300

    Domain Layer Getting Device Info Part2

--- lib/domain/usecase/login_usecase.dart
+++ lib/domain/usecase/login_usecase.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:tut_app/app/functions.dart';
 import 'package:tut_app/data/network/failure.dart';
 import 'package:tut_app/data/request/request.dart';
 import 'package:tut_app/domain/models/models.dart';
@@ -6,19 +7,20 @@ import 'package:tut_app/domain/repository/repository.dart';
 import 'package:tut_app/domain/usecase/base_usecase.dart';
 
 class LoginUseCase implements BaseUseCase<LoginUseCaseInput, Authentication> {
-  Repository _repository;
+  final Repository _repository;
 
   LoginUseCase(this._repository);
 
   @override
   Future<Either<Failure, Authentication>> execute(
       LoginUseCaseInput input) async {
-    await _repository.login(
+    DeviceInfo deviceInfo = await getDeviceDetails();
+    return await _repository.login(
       LoginRequest(
         input.email,
         input.password,
-        "imei",
-        "deviceType",
+        deviceInfo.identifier,
+        deviceInfo.name,
       ),
     );
   }
