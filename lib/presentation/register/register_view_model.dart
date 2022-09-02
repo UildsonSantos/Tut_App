1af06b3025f9a2ca23faeb695842c4390c4750d5
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-09-02 14:03:43 -0300

    Adding Stream Controllers to Register ViewModel

---
+++ lib/presentation/register/register_view_model.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:tut_app/presentation/base/base_view_model.dart';
+
+class RegisterViewModel extends BaseViewModel {
+  final StreamController _userNameStreamController =
+      StreamController<String>.broadcast();
+  final StreamController _mobileNumberStreamController =
+      StreamController<String>.broadcast();
+
+  final StreamController _emailStreamController =
+      StreamController<String>.broadcast();
+
+  final StreamController _passwordStreamController =
+      StreamController<String>.broadcast();
+
+  final StreamController _profilePictureStreamController =
+      StreamController<File>.broadcast();
+
+  final StreamController _isAllInputsValidStreamController =
+      StreamController<void>.broadcast();
+
+  @override
+  void start() {
+    // TODO: implement start
+  }
+
+  @override
+  void dispose() {
+    _isAllInputsValidStreamController.close();
+    _userNameStreamController.close();
+    _mobileNumberStreamController.close();
+    _emailStreamController.close();
+    _passwordStreamController.close();
+    _profilePictureStreamController.close();
+    super.dispose();
+  }
+}
