b6a5d32d25ba0577e91d60e1cf005d018c11f393
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-09-03 01:09:36 -0300

    Adding UI for Register Screen

--- lib/presentation/register/register.dart
+++ lib/presentation/register/register.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:tut_app/app/di.dart';
 import 'package:tut_app/presentation/common/state_renderer/state_render_impl.dart';
 import 'package:tut_app/presentation/register/register_view_model.dart';
+import 'package:tut_app/presentation/resources/assets_manager.dart';
 import 'package:tut_app/presentation/resources/color_manager.dart';
+import 'package:tut_app/presentation/resources/routes_manager.dart';
+import 'package:tut_app/presentation/resources/strings_manager.dart';
 import 'package:tut_app/presentation/resources/values_manager.dart';
 
 class RegisterView extends StatefulWidget {
@@ -17,13 +20,13 @@ class _RegisterViewState extends State<RegisterView> {
   final _formKey = GlobalKey<FormState>();
 
   final TextEditingController _userNameTextEditingController =
-  TextEditingController();
+      TextEditingController();
   final TextEditingController _mobileNumberTextEditingController =
-  TextEditingController();
+      TextEditingController();
   final TextEditingController _userEmailEditingController =
-  TextEditingController();
-  final TextEditingController _userPasswordEditingController =
-  TextEditingController();
+      TextEditingController();
+  final TextEditingController _passwordEditingController =
+      TextEditingController();
 
   @override
   void initState() {
@@ -37,8 +40,8 @@ class _RegisterViewState extends State<RegisterView> {
       _viewModel.setUserName(_userNameTextEditingController.text);
     });
 
-    _userPasswordEditingController.addListener(() {
-      _viewModel.setPassword(_userPasswordEditingController.text);
+    _passwordEditingController.addListener(() {
+      _viewModel.setPassword(_passwordEditingController.text);
     });
 
     _userEmailEditingController.addListener(() {
@@ -81,9 +84,104 @@ class _RegisterViewState extends State<RegisterView> {
   }
 
   Widget _getContentWidget() {
-    return const Center(
-      child: Text('Register Screen'),
-    );
+    return Container(
+        padding: const EdgeInsets.only(top: AppPadding.p100),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const Image(image: AssetImage(ImageAssets.splashLogo)),
+                const SizedBox(height: AppSize.s28),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: AppPadding.p28,
+                    right: AppPadding.p28,
+                  ),
+                  child: StreamBuilder<String?>(
+                    stream: _viewModel.outputErrorUserName,
+                    builder: (context, snapshot) {
+                      return TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _userNameTextEditingController,
+                          decoration: InputDecoration(
+                              hintText: AppStrings.username,
+                              labelText: AppStrings.username,
+                              errorText: snapshot.data));
+                    },
+                  ),
+                ),
+                const SizedBox(height: AppSize.s28),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: AppPadding.p28,
+                    right: AppPadding.p28,
+                  ),
+                  child: StreamBuilder<String?>(
+                    stream: _viewModel.outputErrorPassword,
+                    builder: (context, snapshot) {
+                      return TextFormField(
+                          keyboardType: TextInputType.visiblePassword,
+                          controller: _passwordEditingController,
+                          decoration: InputDecoration(
+                              hintText: AppStrings.password,
+                              labelText: AppStrings.password,
+                              errorText: snapshot.data));
+                    },
+                  ),
+                ),
+                const SizedBox(height: AppSize.s28),
+                Padding(
+                    padding: const EdgeInsets.only(
+                      left: AppPadding.p28,
+                      right: AppPadding.p28,
+                    ),
+                    child: StreamBuilder<bool>(
+                      stream: _viewModel.outputIsAllInputsValid,
+                      builder: (context, snapshot) {
+                        return SizedBox(
+                          width: double.infinity,
+                          height: AppSize.s40,
+                          child: ElevatedButton(
+                              onPressed: (snapshot.data ?? false)
+                                  ? () {
+                                      _viewModel.register();
+                                    }
+                                  : null,
+                              child: const Text(AppStrings.login)),
+                        );
+                      },
+                    )),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: AppPadding.p8,
+                    left: AppPadding.p28,
+                    right: AppPadding.p28,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(
+                              context, Routes.forgotPasswordRoute);
+                        },
+                        child: Text(AppStrings.forgotPassword,
+                            style: Theme.of(context).textTheme.subtitle2),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, Routes.registerRoute);
+                        },
+                        child: Text(AppStrings.registerText,
+                            style: Theme.of(context).textTheme.subtitle2),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
-
