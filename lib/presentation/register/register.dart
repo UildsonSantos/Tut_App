204160847b22dd48b1958edd77744e154b7b3eca
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-09-05 14:48:20 -0300

    Adding UI for Register Screen part2

--- lib/presentation/register/register.dart
+++ lib/presentation/register/register.dart
@@ -1,5 +1,7 @@
+import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:tut_app/app/di.dart';
+import 'package:tut_app/data/mapper/mapper.dart';
 import 'package:tut_app/presentation/common/state_renderer/state_render_impl.dart';
 import 'package:tut_app/presentation/register/register_view_model.dart';
 import 'package:tut_app/presentation/resources/assets_manager.dart';
@@ -23,8 +25,7 @@ class _RegisterViewState extends State<RegisterView> {
       TextEditingController();
   final TextEditingController _mobileNumberTextEditingController =
       TextEditingController();
-  final TextEditingController _userEmailEditingController =
-      TextEditingController();
+  final TextEditingController _emailEditingController = TextEditingController();
   final TextEditingController _passwordEditingController =
       TextEditingController();
 
@@ -44,8 +45,8 @@ class _RegisterViewState extends State<RegisterView> {
       _viewModel.setPassword(_passwordEditingController.text);
     });
 
-    _userEmailEditingController.addListener(() {
-      _viewModel.setEmail(_userEmailEditingController.text);
+    _emailEditingController.addListener(() {
+      _viewModel.setEmail(_emailEditingController.text);
     });
 
     _mobileNumberTextEditingController.addListener(() {
@@ -85,103 +86,175 @@ class _RegisterViewState extends State<RegisterView> {
 
   Widget _getContentWidget() {
     return Container(
-        padding: const EdgeInsets.only(top: AppPadding.p100),
-        child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                const Image(image: AssetImage(ImageAssets.splashLogo)),
-                const SizedBox(height: AppSize.s28),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: AppPadding.p28,
-                    right: AppPadding.p28,
-                  ),
-                  child: StreamBuilder<String?>(
-                    stream: _viewModel.outputErrorUserName,
-                    builder: (context, snapshot) {
-                      return TextFormField(
-                          keyboardType: TextInputType.emailAddress,
-                          controller: _userNameTextEditingController,
-                          decoration: InputDecoration(
-                              hintText: AppStrings.username,
-                              labelText: AppStrings.username,
-                              errorText: snapshot.data));
-                    },
-                  ),
+      padding: const EdgeInsets.only(top: AppPadding.p60),
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              const Image(image: AssetImage(ImageAssets.splashLogo)),
+              const SizedBox(height: AppSize.s28),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: AppPadding.p28,
+                  right: AppPadding.p28,
                 ),
-                const SizedBox(height: AppSize.s28),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: AppPadding.p28,
-                    right: AppPadding.p28,
-                  ),
-                  child: StreamBuilder<String?>(
-                    stream: _viewModel.outputErrorPassword,
-                    builder: (context, snapshot) {
-                      return TextFormField(
-                          keyboardType: TextInputType.visiblePassword,
-                          controller: _passwordEditingController,
-                          decoration: InputDecoration(
-                              hintText: AppStrings.password,
-                              labelText: AppStrings.password,
-                              errorText: snapshot.data));
-                    },
-                  ),
+                child: StreamBuilder<String?>(
+                  stream: _viewModel.outputErrorUserName,
+                  builder: (context, snapshot) {
+                    return TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      controller: _userNameTextEditingController,
+                      decoration: InputDecoration(
+                        hintText: AppStrings.username,
+                        labelText: AppStrings.username,
+                        errorText: snapshot.data,
+                      ),
+                    );
+                  },
                 ),
-                const SizedBox(height: AppSize.s28),
-                Padding(
-                    padding: const EdgeInsets.only(
-                      left: AppPadding.p28,
-                      right: AppPadding.p28,
-                    ),
-                    child: StreamBuilder<bool>(
-                      stream: _viewModel.outputIsAllInputsValid,
-                      builder: (context, snapshot) {
-                        return SizedBox(
-                          width: double.infinity,
-                          height: AppSize.s40,
-                          child: ElevatedButton(
-                              onPressed: (snapshot.data ?? false)
-                                  ? () {
-                                      _viewModel.register();
-                                    }
-                                  : null,
-                              child: const Text(AppStrings.login)),
-                        );
-                      },
-                    )),
-                Padding(
+              ),
+              Center(
+                child: Padding(
                   padding: const EdgeInsets.only(
-                    top: AppPadding.p8,
                     left: AppPadding.p28,
                     right: AppPadding.p28,
+                    bottom: AppPadding.p28,
                   ),
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pushNamed(
-                              context, Routes.forgotPasswordRoute);
-                        },
-                        child: Text(AppStrings.forgotPassword,
-                            style: Theme.of(context).textTheme.subtitle2),
+                      Expanded(
+                        flex: 1,
+                        child: CountryCodePicker(
+                          onChanged: (country) {
+                            // update view model with the selected code
+                            _viewModel
+                                .setCountryCode(country.dialCode ?? empty);
+                          },
+                          initialSelection: "+33",
+                          showCountryOnly: true,
+                          showOnlyCountryWhenClosed: true,
+                          favorite: const ["+966", "+02", "+39", "+55"],
+                        ),
                       ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, Routes.registerRoute);
-                        },
-                        child: Text(AppStrings.registerText,
-                            style: Theme.of(context).textTheme.subtitle2),
+                      Expanded(
+                        flex: 3,
+                        child: StreamBuilder<String?>(
+                          stream: _viewModel.outputErrorMobileNumber,
+                          builder: (context, snapshot) {
+                            return TextFormField(
+                              keyboardType: TextInputType.phone,
+                              controller: _mobileNumberTextEditingController,
+                              decoration: InputDecoration(
+                                hintText: AppStrings.mobileNumber,
+                                labelText: AppStrings.mobileNumber,
+                                errorText: snapshot.data,
+                              ),
+                            );
+                          },
+                        ),
                       )
                     ],
                   ),
-                )
-              ],
-            ),
+                ),
+              ),
+              const SizedBox(height: AppSize.s28),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: AppPadding.p28,
+                  right: AppPadding.p28,
+                ),
+                child: StreamBuilder<String?>(
+                  stream: _viewModel.outputErrorEmail,
+                  builder: (context, snapshot) {
+                    return TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      controller: _emailEditingController,
+                      decoration: InputDecoration(
+                        hintText: AppStrings.emailHint,
+                        labelText: AppStrings.emailHint,
+                        errorText: snapshot.data,
+                      ),
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(height: AppSize.s28),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: AppPadding.p28,
+                  right: AppPadding.p28,
+                ),
+                child: StreamBuilder<String?>(
+                  stream: _viewModel.outputErrorPassword,
+                  builder: (context, snapshot) {
+                    return TextFormField(
+                      keyboardType: TextInputType.visiblePassword,
+                      controller: _passwordEditingController,
+                      decoration: InputDecoration(
+                        hintText: AppStrings.password,
+                        labelText: AppStrings.password,
+                        errorText: snapshot.data,
+                      ),
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(height: AppSize.s28),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: AppPadding.p28,
+                  right: AppPadding.p28,
+                ),
+                child: StreamBuilder<bool>(
+                  stream: _viewModel.outputIsAllInputsValid,
+                  builder: (context, snapshot) {
+                    return SizedBox(
+                      width: double.infinity,
+                      height: AppSize.s40,
+                      child: ElevatedButton(
+                        onPressed: (snapshot.data ?? false)
+                            ? () {
+                                _viewModel.register();
+                              }
+                            : null,
+                        child: const Text(AppStrings.login),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: AppPadding.p8,
+                  left: AppPadding.p28,
+                  right: AppPadding.p28,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(
+                            context, Routes.forgotPasswordRoute);
+                      },
+                      child: Text(AppStrings.forgotPassword,
+                          style: Theme.of(context).textTheme.subtitle2),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, Routes.registerRoute);
+                      },
+                      child: Text(AppStrings.registerText,
+                          style: Theme.of(context).textTheme.subtitle2),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
