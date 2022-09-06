71f4ea29f824b6f1eb5e30f9890cf6b1e769f1a2
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-09-06 16:43:44 -0300

    Adding Home UseCase

---
+++ lib/domain/usecase/home_usecase.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:tut_app/data/network/failure.dart';
+import 'package:tut_app/domain/models/models.dart';
+import 'package:tut_app/domain/repository/repository.dart';
+import 'package:tut_app/domain/usecase/base_usecase.dart';
+
+class HomeUseCase extends BaseUseCase<void, HomeObject> {
+  final Repository _repository;
+
+  HomeUseCase(this._repository);
+
+  @override
+  Future<Either<Failure, HomeObject>> execute(void input) async {
+    return await _repository.getHome();
+  }
+}
