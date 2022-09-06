6ec71ac80ec2ae8f982167f6f90bbd79a4d6e7f4
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-09-06 15:26:53 -0300

    Adding Pages Inside Main View

--- lib/presentation/main/main_view.dart
+++ lib/presentation/main/main_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:tut_app/presentation/main/home_page.dart';
+import 'package:tut_app/presentation/main/notifications_page.dart';
+import 'package:tut_app/presentation/main/search_page.dart';
+import 'package:tut_app/presentation/main/settings_page.dart';
+import 'package:tut_app/presentation/resources/strings_manager.dart';
 
 class MainView extends StatefulWidget {
   const MainView({Key? key}) : super(key: key);
@@ -8,12 +13,25 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
+  List<Widget> pages = [
+    const HomePage(),
+    const SearchPage(),
+    const NotificationsPage(),
+    const SettingsPage()
+  ];
+  final _title = AppStrings.home;
+  final _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Main Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          _title,
+          style: Theme.of(context).textTheme.headline2,
+        ),
       ),
+      body: pages[_currentIndex],
     );
   }
 }
