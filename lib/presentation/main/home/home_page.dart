752f7637485113ff526ef8b73b514739784e45ac
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-09-07 16:39:05 -0300

    Adding Stores Widget

--- lib/presentation/main/home/home_page.dart
+++ lib/presentation/main/home/home_page.dart
@@ -5,6 +5,7 @@ import 'package:tut_app/domain/models/models.dart';
 import 'package:tut_app/presentation/common/state_renderer/state_render_impl.dart';
 import 'package:tut_app/presentation/main/home/home_view_model.dart';
 import 'package:tut_app/presentation/resources/color_manager.dart';
+import 'package:tut_app/presentation/resources/routes_manager.dart';
 import 'package:tut_app/presentation/resources/strings_manager.dart';
 import 'package:tut_app/presentation/resources/values_manager.dart';
 
@@ -189,6 +190,51 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _getStores() {
-    return const Center();
+    return StreamBuilder<List<Store>>(
+        stream: _viewModel.outputStores,
+        builder: (context, snapshot) {
+          return _getStoresWidget(snapshot.data);
+        });
+  }
+
+  Widget _getStoresWidget(List<Store>? stores) {
+    if (stores != null) {
+      return Padding(
+        padding: const EdgeInsets.only(
+          left: AppPadding.p12,
+          right: AppPadding.p12,
+          top: AppPadding.p12,
+        ),
+        child: Flex(
+          direction: Axis.vertical,
+          children: [
+            GridView.count(
+              crossAxisSpacing: AppSize.s8,
+              mainAxisSpacing: AppSize.s8,
+              physics: const ScrollPhysics(),
+              shrinkWrap: true,
+              crossAxisCount: AppSize.si2,
+              children: List.generate(stores.length, (index) {
+                return InkWell(
+                  onTap: () {
+                    // navigate to store details screen
+                    Navigator.of(context).pushNamed(Routes.storeDetailsRoute);
+                  },
+                  child: Card(
+                    elevation: AppSize.s4,
+                    child: Image.network(
+                      stores[index].image,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              }),
+            )
+          ],
+        ),
+      );
+    } else {
+      return Container();
+    }
   }
 }
