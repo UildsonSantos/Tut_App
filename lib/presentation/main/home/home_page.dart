b8306c1b03156af802d8b4c1cdd96d7fdba27aaf
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-09-07 15:54:45 -0300

    Home View UI

--- lib/presentation/main/home/home_page.dart
+++ lib/presentation/main/home/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:tut_app/app/di.dart';
+import 'package:tut_app/presentation/common/state_renderer/state_render_impl.dart';
 import 'package:tut_app/presentation/main/home/home_view_model.dart';
 import 'package:tut_app/presentation/resources/strings_manager.dart';
+import 'package:tut_app/presentation/resources/values_manager.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -31,8 +33,58 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(AppStrings.home),
+    return Center(
+      child: SingleChildScrollView(
+        child: StreamBuilder<FlowState>(
+          stream: _viewModel.outputState,
+          builder: (context, snapshot) {
+            return snapshot.data?.getScreenWidget(context, _getContentWidgets(),
+                    () {
+                  _viewModel.start();
+                }) ??
+                Container();
+          },
+        ),
+      ),
     );
+  }
+
+  Widget _getContentWidgets() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _getBannersCarousel(),
+        _getSection(AppStrings.services),
+        _getServices(),
+        _getSection(AppStrings.stores),
+        _getStores()
+      ],
+    );
+  }
+
+  Widget _getSection(String title) {
+    return Padding(
+      padding: const EdgeInsets.only(
+          top: AppPadding.p12,
+          left: AppPadding.p12,
+          right: AppPadding.p12,
+          bottom: AppPadding.p2),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headline3,
+      ),
+    );
+  }
+
+  Widget _getBannersCarousel() {
+    return const Center();
+  }
+
+  Widget _getServices() {
+    return const Center();
+  }
+
+  Widget _getStores() {
+    return const Center();
   }
 }
