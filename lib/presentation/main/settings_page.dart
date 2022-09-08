556d9f53971eaf318acb8e760bf8788a34e57d8d
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-09-08 09:05:48 -0300

    Adding UI of Settings Screen

--- lib/presentation/main/settings_page.dart
+++ lib/presentation/main/settings_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tut_app/presentation/resources/assets_manager.dart';
 import 'package:tut_app/presentation/resources/strings_manager.dart';
+import 'package:tut_app/presentation/resources/values_manager.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -11,8 +14,46 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(AppStrings.settings),
+    return ListView(
+      padding: const EdgeInsets.all(AppPadding.p8),
+      children: [
+        ListTile(
+          title: Text(
+            AppStrings.changeLanguage,
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          leading: SvgPicture.asset(ImageAssets.changeLangIc),
+          trailing: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          onTap: () {},
+        ),
+        ListTile(
+          title: Text(
+            AppStrings.contactUs,
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          leading: SvgPicture.asset(ImageAssets.contactUsIc),
+          trailing: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          onTap: () {},
+        ),
+        ListTile(
+          title: Text(
+            AppStrings.inviteYourFriends,
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          leading: SvgPicture.asset(ImageAssets.inviteFriendsIc),
+          trailing: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          onTap: () {},
+        ),
+        ListTile(
+          title: Text(
+            AppStrings.logout,
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          leading: SvgPicture.asset(ImageAssets.logoutIc),
+          trailing: SvgPicture.asset(ImageAssets.settingsRightArrowIc),
+          onTap: () {},
+        )
+      ],
     );
   }
 }
