b5436b899505e5ef7c100b1c838cd9d684a0ed46
Uildson S. dos Santos <wilder_shon@hotmail.com>
2022-09-08 14:59:53 -0300

    Update Main function with Localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:tut_app/app/app.dart';
+import 'package:tut_app/presentation/resources/language_manager.dart';
 
 import 'app/di.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   await initAppModule();
-  runApp(MyApp());
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [englishLocal, arabicLocal],
+      path: assetsPathLocalization,
+      child: Phoenix(child: MyApp()),
+    ),
+  );
 }
